e3d9051a42dd5ab4bbd4875f4bf61cb68988be1b
Enzo Lionel <90061668+Enzoolionel@users.noreply.github.com>
2022-11-07 13:24:09 -0300

    Initial commit

---
+++ src/com/tema1/TemaOne.java
@@ -0,0 +1,20 @@
+package com.tema1;
+
+public class TemaOne {
+  public static void main(String[] args) {
+
+
+    int numeroInt = 5;
+    long numeroLong = 123123;
+    double numeroDouble = 22.22;
+    boolean tipoBoolean = true;
+    String cadenaStr = "Hola mundo";
+
+    System.out.println(numeroInt);
+    System.out.println(numeroLong);
+    System.out.println(numeroDouble);
+    System.out.println(tipoBoolean);
+    System.out.println(cadenaStr);
+  }
+
+}
