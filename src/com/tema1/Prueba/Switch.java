06f1e743f2953104ead2200b31a5346d8a4d3d19
Enzo Lionel <90061668+Enzoolionel@users.noreply.github.com>
2022-11-09 13:44:50 -0300

    concatenarTexto

---
+++ src/com/tema1/Prueba/Switch.java
@@ -0,0 +1,29 @@
+package com.tema1.Prueba;
+
+public class Switch {
+  public static void main(String[] args) {
+//    String dia = "sabado";
+
+//    switch (dia){
+//      case "lunes":
+//      case "martes":
+//      case "miercoles":
+//      case "jueves":
+//      case "viernes":
+//        System.out.println("Es dia de semana.");
+//        break;
+//      case "sabado":
+//      case "domingo":
+//        System.out.println("Es fin de semana.");
+//        break;
+//    }
+//    -------------------------------------------------
+
+    String[] nombres = {"enzo", "lionel", "liliana", "beatriz", "emma", "sofia"};
+
+    for (int i = 0; i < nombres.length; i++){
+      System.out.println(nombres[i] + i);
+    }
+
+  }
+}
